5d3349f52cf910e6fa13b89fcd89c72f0a72f1d5
westford14 <westford14@gmail.com>
2025-05-05 11:11:22 +0200

    fixing test

--- tests/common/constants.rs
+++ tests/common/constants.rs
@@ -8,4 +8,4 @@ pub const API_PATH_TRANSACTIONS: &str = "transactions";
 
 pub const TEST_ADMIN_USERNAME: &str = "admin";
 pub const TEST_ADMIN_PASSWORD_HASH: &str =
-    "7c44575b741f02d49c3e988ba7aa95a8fb6d90c0ef63a97236fa54bfcfbd9d51";
+    "$2b$10$/4ROWmG8t2es761Z.ViF1ODWu50dJLN3OXMoxNE.XlF0nitkbQKaq";
